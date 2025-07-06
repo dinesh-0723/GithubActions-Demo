44800880b3d5a159581033b69c8cd588dd3b0e0c
Dcloud <dcloud23@outlook.com>
2025-07-06 09:42:44 +0530

    completed

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,9 @@
 provider "azurerm" {
+  subscription_id = "dc70695c-b344-48c0-b8c4-02b002f254ce"
+  tenant_id       = "81c3b323-8a08-4730-b416-9066c1bea520"   # Required for many scenarios
   features {}
 }
+
 
 resource "azurerm_resource_group" "rg" {
   name     = "github-rg"
