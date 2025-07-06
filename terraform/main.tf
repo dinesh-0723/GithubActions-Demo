46986db0ae5dc2bd8bba2c807927f2f92e6af703
Dcloud <dcloud23@outlook.com>
2025-07-06 11:03:09 +0530

    completed

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "githubactions-acr"      # Must be globally unique and 5-50 alphanumeric characters
+  name                = "githubactionsacr"      # Must be globally unique and 5-50 alphanumeric characters
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Premium"                   # Options: Basic, Standard, Premium
