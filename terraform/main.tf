116c9ab71e24908859e49ba47b2e6d4b5418008c
Dcloud <dcloud23@outlook.com>
2025-07-06 09:49:06 +0530

    completed

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "github-rg"
   location = "East US"
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "myuniqueregistryname"      # Must be globally unique and 5-50 alphanumeric characters
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"                   # Options: Basic, Standard, Premium
+  admin_enabled       = false                       # Set true to enable admin user (not recommended for production)
+}
