eb7495f6188752bc9541a12c639fa28674af7637
Dcloud <dcloud23@outlook.com>
2025-07-06 13:22:30 +0530

    completed

--- terraform/vnet/main.tf
+++ terraform/vnet/main.tf
@@ -13,4 +13,5 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = data.azurerm_resource_group.extrg.location
   resource_group_name = data.azurerm_resource_group.extrg.name
+  tags = "dev"
 }
