50135592ac626f8e8a3b321db3dd2b9aa0edb74d
Dcloud <dcloud23@outlook.com>
2025-07-06 13:49:15 +0530

    completed

---
+++ terraform/vnet/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "your_resource_group_name"
+    storage_account_name  = "stgac01"
+    container_name        = "stgaccr01"
+    key                   = "vnet.tfstate"
+    tenant_id            = "81c3b323-8a08-4730-b416-9066c1bea520"
+    subscription_id      = "dc70695c-b344-48c0-b8c4-02b002f254ce"
+  }
+}
--- terraform/vnet/main.tf
+++ terraform/vnet/main.tf
@@ -13,9 +13,9 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = data.azurerm_resource_group.extrg.location
   resource_group_name = data.azurerm_resource_group.extrg.name
-    tags = {
-    Environment = "dev"
-    Owner       = "MySelf"
-    Project     = "MyOwnProject"
-  }
+  #   tags = {
+  #   Environment = "dev"
+  #   Owner       = "MySelf"
+  #   Project     = "MyOwnProject"
+  # }
 }
